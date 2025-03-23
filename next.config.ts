75ac97f91a93a093f0c341d0e05c6795e39d1178
Efrain Lemus <efraintlemusm@gmail.com>
2025-03-23 17:14:28 -0600

    removing unnecessary files

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,14 @@ const nextConfig = {
     },
   },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
