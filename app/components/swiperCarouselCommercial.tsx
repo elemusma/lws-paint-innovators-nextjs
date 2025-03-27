ac7b79c621b683cfa118d2236f05e1dfca25450a
Efrain Lemus <efraintlemusm@gmail.com>
2025-03-26 22:20:42 -0600

    setting up hero carousel to be more commercial like

--- app/components/swiperCarouselCommercial.tsx
+++ app/components/swiperCarouselCommercial.tsx
@@ -25,7 +25,7 @@ const SwiperCarouselCommercial = () => {
           speed={1500} // Increased speed for slower transition
         >
           <SwiperSlide>
-            <div className="relative flex min-h-[76vh] w-full justify-center pt-[100px] overflow-hidden">
+            <div className="relative flex min-h-[81vh] w-full justify-center pt-[100px] overflow-hidden">
               <Image
                 src="/home/Paint-Innovators-2025-12.jpg"
                 alt="Paint Innovators Industrial"
@@ -43,9 +43,9 @@ const SwiperCarouselCommercial = () => {
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="relative flex min-h-[76vh] w-full justify-center pt-[100px] overflow-hidden">
+            <div className="relative flex min-h-[81vh] w-full justify-center pt-[100px] overflow-hidden">
               <Image
-                src="/assets/Paint-Innovators-Industrial.jpg"
+                src="/home/Paint-Innovators-2025-07.jpg"
                 alt="Paint Innovators Industrial"
                 style={{ objectFit: "cover" }}
                 className="absolute top-0 left-0 w-full h-full kenburns"
@@ -61,7 +61,7 @@ const SwiperCarouselCommercial = () => {
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="relative flex min-h-[76vh] w-full justify-center pt-[100px] overflow-hidden">
+            <div className="relative flex min-h-[81vh] w-full justify-center pt-[100px] overflow-hidden">
               <Image
                 src="/home/Paint-Innovators-2025-01.jpg"
                 alt="Paint Innovators Industrial"
@@ -79,7 +79,7 @@ const SwiperCarouselCommercial = () => {
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="relative flex min-h-[76vh] w-full justify-center pt-[100px] overflow-hidden">
+            <div className="relative flex min-h-[81vh] w-full justify-center pt-[100px] overflow-hidden">
               <Image
                 src="/home/Paint-Innovators-Commercial-02.jpg"
                 alt="Paint Innovators Industrial"
@@ -96,20 +96,63 @@ const SwiperCarouselCommercial = () => {
               ></div>
             </div>
           </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative flex min-h-[81vh] w-full justify-center pt-[100px] overflow-hidden">
+              <Image
+                src="/home/Paint-Innovators-Commercial-01.jpg"
+                alt="Paint Innovators Industrial"
+                style={{ objectFit: "cover" }}
+                className="absolute top-0 left-0 w-full h-full kenburns"
+                fill
+              />
+              <div
+                className="absolute top-0 left-0 w-full h-full"
+                style={{
+                  background:
+                    "linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.7049413515406162) 82%)",
+                }}
+              ></div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative flex min-h-[81vh] w-full justify-center pt-[100px] overflow-hidden">
+              <Image
+                src="/home/Paint-Innovators-2025-06.jpg"
+                alt="Paint Innovators Industrial"
+                style={{ objectFit: "cover" }}
+                className="absolute top-0 left-0 w-full h-full kenburns"
+                fill
+              />
+              <div
+                className="absolute top-0 left-0 w-full h-full"
+                style={{
+                  background:
+                    "linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.7049413515406162) 82%)",
+                }}
+              ></div>
+            </div>
+          </SwiperSlide>
         </Swiper>
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 py-[250px]">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 py-[250px] lg:w-3/4 w-full px-4">
           <div className="relative flex w-full text-white z-10">
             <div
               style={{ background: "red", minWidth: "5px" }}
               className="w-[5px] h-[100px] mr-[10px]"
             ></div>
             <div>
-              <h1 className="h1 mb-[15px] text-shadow mt-[-8px] font-aspira-light uppercase">
-                Commercial Painting
+              <h1 className="mb-[15px] text-shadow mt-[-8px] font-proxima-bold uppercase">
+                Your Preferred Painter for Commercial &amp; Industrial Projects
               </h1>
-              <p className="text-shadow">
-                Enhance your business with Paint Innovators&apos; professional
-                commercial painting services across Arkansas and Oklahoma.
+              {/* <h2 className="font-aspira-bold text-2xl text-shadow">
+                Arkansas <span className="text-accent">&bull;</span> Oklahoma{" "}
+                <span className="text-accent">&bull;</span> Colorado
+              </h2> */}
+              <p className="text-shadow text-xl">
+                <strong>
+                  Whether it&apos;s new construction or renovations, our expert
+                  team ensures a flawless finish that enhances durability,
+                  boosts property value, and leaves a lasting impression.
+                </strong>
               </p>
               <Link href="/get-estimate" className="btn-main mt-[20px]">
                 Get Free Estimate
