aab1878499c1732c489767520bb48fdacec71449
Efrain Lemus <efraintlemusm@gmail.com>
2025-04-02 01:02:35 -0600

    remove team photo and add videos

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,6 @@ export default function Home() {
     <>
       <div className="hero">
         <SwiperCarouselCommercial />
-        {/* <SwiperCarousel /> */}
       </div>
       <ContentBlock
         sectionClassName="bg-accent text-white"
@@ -58,7 +57,6 @@ export default function Home() {
           </>
         }
       >
-        {/* <div className="flex flex-col lg:flex-row items-center relative"> */}
         <div className="w-full md:w-1/2 lg:pr-10 pb-10 sm:pb-0">
           <div
             className="divider"
@@ -82,14 +80,14 @@ export default function Home() {
           </Link>
         </div>
         <div className="w-full md:w-1/2 lg:text-right">
-          <Image
+          {/* <Image
             src={`/team/Paint-Innovators-Team-Home.jpg`}
             alt={`Paint Innovators Team`}
             width={1000}
             height={1000}
             className="object-cover"
-          />
-          {/* <iframe
+          /> */}
+          <iframe
             src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FPaintInnovators%2Fvideos%2F979154837397440%2F&show_text=false&width=267&t=0"
             width="50%"
             height="476"
@@ -102,8 +100,8 @@ export default function Home() {
             frameBorder="0"
             allowFullScreen
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-          ></iframe> */}
-          {/* <iframe
+          ></iframe>
+          <iframe
             src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FPaintInnovators%2Fvideos%2F1098726621971511%2F&show_text=false&width=267&t=0"
             width="50%"
             height="476"
@@ -116,72 +114,20 @@ export default function Home() {
             frameBorder="0"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
             allowFullScreen
-          ></iframe> */}
+          ></iframe>
         </div>
-        {/* </div> */}
       </ContentBlock>
       <div className="spacer"></div>
       <CoreValues />
-      {/* <ContentBlock
-        sectionClassName=""
-        containerClassName="max-w-screen-lg mx-auto py-3"
-        rowClassName="flex flex-col items-center"
-        columnClassName="w-full text-center"
-      >
-        <h2 className="text-5xl font-aspira-thin">INDUSTRIES SERVED</h2>
-      </ContentBlock> */}
-      {/* <Tabs
-        tabs={tabsData}
-        containerClassName="my-custom-container max-w-screen-lg mx-auto"
-        headerClassName="flex flex-wrap justify-center mb-4 px-4 headerClassName"
-        tabButtonClassName="px-4 py-2 border mr-2"
-        activeTabButtonClassName="bg-blue-600 text-white activeTabButtonClassName"
-        contentClassName="p-4"
-      /> */}
+
       <div className="spacer"></div>
       <SwiperCarouselNewService />
       <div className="spacer"></div>
 
       <CompletedProjects />
-      {/* <ContentBlock
-        sectionClassName="bg-black text-white completed-projects py-[30px]"
-        containerClassName="max-w-screen-lg mx-auto py-8"
-        rowClassName="flex flex-col items-center"
-        columnClassName="w-full text-center"
-      >
-        <h2 className="text-4xl font-aspira-thin mb-4">COMPLETED PROJECTS</h2>
-        <div className="relative swiper-carousel-container">
-          <SwiperCarouselProjects
-            slides={ResidentialProjectsImages}
-            sliderId="completed-projects-residential"
-          />
-          <h2 className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 z-10 text-shadow font-proxima-bold md:text-5xl text-3xl tracking-[0.2em] pointer-events-none">
-            RESIDENTIAL
-          </h2>
-        </div>
-        <div className="relative pt-[30px] swiper-carousel-container">
-          <SwiperCarouselProjects
-            slides={CommercialProjectsImages}
-            sliderId="completed-projects-commercial"
-          />
-          <h2 className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 z-10 text-shadow font-proxima-bold md:text-5xl text-3xl tracking-[0.2em] pointer-events-none">
-            COMMERCIAL
-          </h2>
-        </div>
-        <div className="relative pt-[30px] swiper-carousel-container">
-          <SwiperCarouselProjects
-            slides={IndustrialProjectsImages}
-            sliderId="completed-projects-industrial"
-          />
-          <h2 className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 z-10 text-shadow font-proxima-bold md:text-5xl text-3xl tracking-[0.2em] pointer-events-none">
-            INDUSTRIAL
-          </h2>
-        </div>
-      </ContentBlock> */}
+
       <div className="spacer"></div>
 
-      {/* <Testimonials />
-      <div className="spacer"></div> */}
       <CTA />
 
       <div className="spacer"></div>
