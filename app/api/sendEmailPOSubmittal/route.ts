fe78d1e90af5eb4da297ba312b2260adce560e13
Efrain Lemus <efraintlemusm@gmail.com>
2025-04-03 16:46:50 -0600

    removing email recipient to test out emails, need to install recaptcha

--- app/api/sendEmailPOSubmittal/route.ts
+++ app/api/sendEmailPOSubmittal/route.ts
@@ -30,7 +30,7 @@ export async function POST(req: Request) {
 
     const mailOptions = {
       from: `"Latino Web Studio" <${process.env.GMAIL_USER}>`,
-      to: "info@latinowebstudio.com,paintinnovators.it@outlook.com",
+      to: "info@latinowebstudio.com",
       subject: `PO Submittal: "${first_name}"`,
       html: `<table style="background-color: #f7f7f7; width: 100%;">
 <tbody>
